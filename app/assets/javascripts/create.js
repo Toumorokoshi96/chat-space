8f366fff48b2cd61d00c88e98cc092d19f379da5
toumorokoshi96 <chucchuchu2019@icloud.com>
2019-09-28 14:53:18 +0900

    Asynchronous posting has succeeded

--- app/assets/javascripts/create.js
+++ app/assets/javascripts/create.js
@@ -1,6 +1,45 @@
 $(function(){
+  function messageHTMLgenerater(message){
+    var html = `
+    <div class='message' 'message_id' = id>
+    <div class='message__upper-info'>
+    <p class='message__upper-info__talker'>
+     name
+    </p>
+    <p class='message__upper-info__date'>
+     date
+    </p>
+    </div>
+    <p class='message__text'>
+     text
+    </p>
+    <div class='message-content'>
+     image
+    </div>
+    </div>`
+
+  return html;
+  }
   $('#new_message').on('submit', function(e){
     e.preventDefault();
-    console.log('Hello');
+    var formData = new FormData(this);
+    var uri = $(this).attr('action');
+    $.ajax({
+      url: uri,
+      type: "POST",
+      data: formData,
+      dataType: 'json', 
+      processData: false,
+      contentType: false
+    })
+    .done(function(message){
+    var messageHTML = messageHTMLgenerater(message);
+    //  var messageHTML = messageHTML(message);
+     var messages = $(".messages");
+     messages.append(messageHTML);
+    })
+    .fail(function(){
+    alert.apply('error')
+    })
   })
 })
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -10,15 +10,32 @@ class MessagesController < ApplicationController
 
   def create
     @message = @group.messages.new(message_params)
-    if @message.save
-      redirect_to group_messages_path(@group), notice: 'メッセージが送信されました'
-    else
-      @messages = @group.messages.includes(:user)
-      flash.now[:alert] = 'メッセージを入力してください。'
-      # flashはページ切り替えに伴う簡易メッセージを表示するメソッド
-      #他にflash.keepやflash[:noticeなどがある]
-      render :index
-    end
+    # ################################################################
+    # if @message.save
+    #   redirect_to group_messages_path(@group), notice: 'メッセージが送信されました'
+    # else
+    #   @messages = @group.messages.includes(:user)
+    #   flash.now[:alert] = 'メッセージを入力してください。'
+    #   # flashはページ切り替えに伴う簡易メッセージを表示するメソッド
+    #   #他にflash.keepやflash[:noticeなどがある]
+    #   render :index
+    # end
+    ###########################################################
+      if @message.save
+        respond_to do |format|
+          format.html {redirect_to group_messages_path(@group), notice: 'メッセージが送信されました'}
+          format.json 
+          # format.json {@message = Message.find(@message.id) # @group.messages.new(message_params)ではid等が空白のはず saveが更新してくれていたりする。。？
+          #             render json: @message} # まずはJbuilerなしでこれでjsonに変換してくれるっぽい
+        end
+      else
+        respond_to do |format|
+         format.html {@messages = @group.messages.includes(:user) #この行の意味よくわからない、がとりあえずひきつぐ render :indexのためだっけ？
+                      flash.now[:alert] = 'メッセージを入力してください。'
+                      render :index}
+         format.json  # js側でエラーを拾うので何も書かなくて良いのだろう
+        end
+      end
   end
 
   def message_params
