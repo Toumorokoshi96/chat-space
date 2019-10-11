f71c2bedc325580daa7cce48138b3248b2db750a
toumorokoshi96 <chucchuchu2019@icloud.com>
2019-10-11 09:51:57 +0900

    correction of settig file

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 # config valid for current version and patch releases of Capistrano
 lock '3.11.2'
 
-set :application, 'chaat-space'
+set :application, 'chat-space'
 set :repo_url, 'git@github.com:Toumorokoshi96/chat-space.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
